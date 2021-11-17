b11a56163fbe0db109a2c2fa591584be9de1e40a
Greg Teixeira <greg.teixeira123@gmail.com>
2021-11-17 09:52:21 -0300

    feat: create routes as a variable

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import Home from "../pages/Home";
+import Product from "../pages/Product";
+import Products from "../pages/Products";
+
+const mainRoutes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "products",
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+      {
+        path: ":productId",
+        element: <Product />,
+      },
+    ],
+  },
+];
+
+export default mainRoutes;
