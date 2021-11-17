70556c2034c9f0f998e2758a7f1b7d19208a6d15
Greg Teixeira <greg.teixeira123@gmail.com>
2021-11-17 09:53:22 -0300

    refactor: update App to router v6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link, useRoutes } from "react-router-dom";
 
-import Routes from './Routes';
+import routes from "./routes";
+import Routes from "./Routes";
 
 function App() {
+  const routeResult = useRoutes(routes);
   return (
-    <BrowserRouter>
+    <>
       <header>
-        <strong>React Router v5</strong>
+        <strong>React Router v6</strong>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/products">Produtos</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
           </ul>
         </nav>
       </header>
 
       <main>
+        {/* You can use by useRoutes like this (I prefer it): */}
+        {/* {routeResult} */}
+        {/* Or use by defining it */}
         <Routes />
       </main>
-    </BrowserRouter>
+    </>
   );
 }
 
